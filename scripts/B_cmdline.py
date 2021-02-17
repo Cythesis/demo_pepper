0e51a50afc162aa2589e73776a06af411557e430
Cythesis <52483870+Cythesis@users.noreply.github.com>
2021-02-18 08:02:51 +1100

    Update B_cmdline.py

--- scripts/B_cmdline.py
+++ scripts/B_cmdline.py
@@ -6,36 +6,6 @@ import argparse
 import sys
 import time
 import pdb
-import scipy.misc
-import numpy
-
-# '/home/nao/.local/share/Explorer/2021-01-13T061256.268Z.explo' r5 path
-# '/home/nao/.local/share/Explorer/2021-01-14T012852.035Z.explo' r5 base path
-
-class Localization:
-	def explore(self, radius):
-		return navigation.explore(radius)
-
-	def saveMap(self):
-		return navigation.saveExploration()
-
-	def saveMapToImage(self, name):
-		result_map = navigation.getMetricalMap()
-		map_width = result_map[1]
-		map_height = result_map[2]
-		img = numpy.array(result_map[4]).reshape(map_width, map_height)
-		img = (100 - img) * 2.55 
-		img = numpy.array(img, numpy.uint8)
-		scipy.misc.imsave(name, img)
-
-	def stop(self):
-		navigation.stopExploration()
-
-	def loadAndLocalize(self,path,guess):
-		navigation.loadExploration(path)
-		navigation.relocalizeInMap(guess)
-		navigation.startLocalization()
-
 
 # Initialize app
 try:
@@ -54,7 +24,6 @@ def start(service, session = session):
 faceCharacteristic = start('ALFaceCharacteristics')
 notification = start('ALNotificationManager')
 autonomousLife = start('ALAutonomousLife')
-
 callback = start('ALExpressionWatcher')
 speech = start('ALSpeechRecognition')
 tactile = start('ALTactileGesture')
@@ -74,13 +43,9 @@ def battery(tts = tts, battery = battery):
 
 def onNotification(value):
 	print(notification.notifications())
-
-lmap = Localization()
+# "Subscribe" to notification changes
 expression_obj2 = callback.add("'NotificationAdded'",2)
 signal_id2 = expression_obj2.signal.connect(onNotification)
-
-
-# Functions to make things easier
 
 # Breakpoint
 pdb.set_trace()
